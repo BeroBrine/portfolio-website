81ba565797537f3a5fd35bac8d08f68060468ce3
Savy011 <savy.novachez011@proton.me>
2024-06-21 18:06:38 +0530

    fix styles at /linux route

--- src/components/LapMockup.tsx
+++ src/components/LapMockup.tsx
@@ -2,14 +2,16 @@ import terminalHello from "../assets/myPics/terminal.mp4";
 
 const LapMockup = () => {
 	return (
-		<div className="px-2 py-2">
-			<div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[172px] max-w-[301px] md:h-[294px] md:max-w-[512px]">
-				<div className="rounded-lg overflow-hidden h-[156px] md:h-[278px] bg-white dark:bg-gray-800">
-					<video src={terminalHello} loop autoPlay></video>
+		<div className="relative px-2 py-2 h-screen">
+			<div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-screen">
+				<div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl h-[208px] aspect-[1.55/1] sm:h-[336px] md:h-[464px]">
+					<div className="rounded-lg overflow-hidden h-full bg-white dark:bg-gray-800">
+						<video src={terminalHello} loop autoPlay></video>
+					</div>
 				</div>
-			</div>
-			<div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] max-w-[351px] md:h-[21px] md:max-w-[597px]">
-				<div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[56px] h-[5px] md:w-[96px] md:h-[8px] bg-gray-800"></div>
+				<div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm h-[17px] sm:h-[19px] md:h-[21px] w-[calc(322px+12%)] sm:w-[calc(521px+12%)] md:w-[calc(719px+12%)]">
+					<div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl h-[5px] sm:h-[6.5px] md:h-[8px] w-[56px] md:w-[96px]  bg-gray-800"></div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Navbar/Linux.tsx
+++ src/components/Navbar/Linux.tsx
@@ -1,18 +1,16 @@
 import LapMockup from "../LapMockup";
 import Navbar from "../Navbar";
 
+
 const Linux = () => {
 	return (
 		<div>
 			<Navbar homepage={false} showByDefault={false} />
-			<div>
-				<img
-					className=" absolute h-screen w-full -z-10"
-					src="https://png.pngtree.com/background/20230401/original/pngtree-cyberpunk-city-purple-background-picture-image_2252484.jpg"
-					alt="evening"
-				/>
-			</div>
-			<div className="backdrop-filter backdrop-blur-sm bg-opacity-10 h-screen w-full z-0 absolute"></div>
+			<img
+				className="absolute object-cover h-screen w-full -z-10 blur-sm"
+				src="/cyberpunk_city.jpeg"
+				alt="evening"
+			/>
 			<LapMockup />
 		</div>
 	);
